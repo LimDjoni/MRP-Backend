19e76e7a9ce2bf1bedf134b90faf90fa355cbd5c
ifantrifandi <ifan.trifandi@gmail.com>
2022-09-14 15:20:33 +0700

    fixing validation date validation'

--- validatorfunc/validator.go
+++ validatorfunc/validator.go
@@ -8,11 +8,24 @@ import (
 func CheckDateString(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
 
+	isLayoutDateErr := false
+	isLayoutTimeErr := false
 	layout := "2006-01-02"
+	layoutTime := "2006-01-02T00:00:00Z"
 	_, err := time.Parse(layout, value)
+	_, errTime := time.Parse(layoutTime, value)
 
 	if err != nil {
-		return false
+		isLayoutDateErr = true
 	}
-	return true
+
+	if errTime != nil {
+		isLayoutTimeErr = true
+	}
+
+	if isLayoutDateErr == false || isLayoutTimeErr == false {
+		return true
+	}
+
+	return false
 }
