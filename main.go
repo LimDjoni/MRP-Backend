daa161dd6766950eed59cfeda6393f7cdbdab7ab
ifantrifandi <ifan.trifandi@gmail.com>
2022-10-05 15:08:21 +0700

    add company routing

--- main.go
+++ main.go
@@ -145,4 +145,5 @@ func Setup(db *gorm.DB, validate *validator.Validate, route fiber.Router) {
 	routing2.MinerbaRouting(db, route, validate)
 	routing2.DmoRouting(db, route, validate)
 	routing2.TraderRouting(db, route, validate)
+	routing2.CompanyRouting(db, route, validate)
 }
---
+++ routing/company.go
@@ -0,0 +1,49 @@
+package routing
+
+import (
+	"ajebackend/handler"
+	"ajebackend/helper"
+	"ajebackend/model/company"
+	"ajebackend/model/logs"
+	"ajebackend/model/trader"
+	"ajebackend/model/user"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
+	"gorm.io/gorm"
+)
+
+func CompanyRouting(db *gorm.DB, app fiber.Router, validate *validator.Validate) {
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	traderRepository := trader.NewRepository(db)
+	traderService := trader.NewService(traderRepository)
+
+	companyRepository := company.NewRepository(db)
+	companyService := company.NewService(companyRepository)
+
+	logRepository := logs.NewRepository(db)
+	logService := logs.NewService(logRepository)
+
+	companyHandler := handler.NewCompanyHandler(userService, companyService, traderService, logService, validate)
+
+	companyRouting := app.Group("/company")
+
+	companyRouting.Use(jwtware.New(jwtware.Config{
+		SigningKey:    []byte(helper.GetEnvWithKey("JWT_SECRET_KEY")),
+		SigningMethod: jwtware.HS256,
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			return ctx.Status(401).JSON(fiber.Map{
+				"error": "unauthorized",
+				"err": err.Error(),
+			})
+		},
+	}))
+
+	companyRouting.Get("/", companyHandler.ListCompany)
+	companyRouting.Post("/create", companyHandler.CreateCompany)
+	companyRouting.Put("/update", companyHandler.UpdateCompany)
+	companyRouting.Delete("/delete/:id", companyHandler.DeleteCompany)
+	companyRouting.Get("/detail/:id", companyHandler.DetailCompany)
+}
