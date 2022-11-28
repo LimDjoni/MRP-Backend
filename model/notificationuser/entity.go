217a3be27833eec35920b51a1496832f10dea59a
ifantrifandi <ifan.trifandi@gmail.com>
2022-11-28 10:52:29 +0700

    save notification user

--- model/notificationuser/entity.go
+++ model/notificationuser/entity.go
@@ -3,14 +3,15 @@ package notificationuser
 import (
 	"ajebackend/model/notification"
 	"ajebackend/model/user"
+
 	"gorm.io/gorm"
 )
 
 type NotificationUser struct {
 	gorm.Model
-	NotificationId uint `json:"notification_id"`
-	Notification notification.Notification `json:"notification" gorm:"constraint:OnDelete:CASCADE;"`
-	IsRead bool `json:"is_read"`
-	UserId uint `json:"user_id"`
-	User user.User `json:"user"`
+	NotificationId uint                      `json:"notification_id"`
+	Notification   notification.Notification `json:"notification" gorm:"constraint:OnDelete:CASCADE;"`
+	IsRead         bool                      `json:"is_read"`
+	UserId         uint                      `json:"user_id"`
+	User           user.User                 `json:"user"`
 }
