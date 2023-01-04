3e2348de2ba49ded963ce2372706de0da6956dd8
ifantrifandi <ifan.trifandi@gmail.com>
2023-01-04 09:59:03 +0700

    add industry type in company

--- model/company/entity.go
+++ model/company/entity.go
@@ -5,8 +5,9 @@ import "gorm.io/gorm"
 type Company struct {
 	gorm.Model
 	CompanyName  string `json:"company_name"`
-	Address     string	`json:"address"`
-	Province	string	`json:"province"`
-	PhoneNumber	string	`json:"phone_number"`
-	FaxNumber	string	`json:"fax_number"`
+	IndustryType string `json:"industry_type"`
+	Address      string `json:"address"`
+	Province     string `json:"province"`
+	PhoneNumber  string `json:"phone_number"`
+	FaxNumber    string `json:"fax_number"`
 }
