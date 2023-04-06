c15b63234e3664cea9fb1350aee0e68f05ceba1a
ifantrifandi <ifan.trifandi@gmail.com>
2023-04-06 13:33:17 +0700

    fixing code update document transaction

--- model/master/allmaster/service.go
+++ model/master/allmaster/service.go
@@ -1,7 +1,10 @@
 package allmaster
 
+import "ajebackend/model/master/iupopk"
+
 type Service interface {
 	ListMasterData() (MasterData, error)
+	FindIupopk(iupopkId int) (iupopk.Iupopk, error)
 }
 
 type service struct {
@@ -16,4 +19,10 @@ func (s *service) ListMasterData() (MasterData, error) {
 	listMasterData, listMasterDataErr := s.repository.ListMasterData()
 
 	return listMasterData, listMasterDataErr
+}
+
+func (s *service) FindIupopk(iupopkId int) (iupopk.Iupopk, error) {
+	findIupopk, findIupopkErr := s.repository.FindIupopk(iupopkId)
+
+	return findIupopk, findIupopkErr
 }
