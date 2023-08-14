b6ebab7f765b7cbbf08fbe9ad23f3b704e1a826b
ifantrifandi <ifan.trifandi@gmail.com>
2023-08-14 14:09:33 +0700

    add preload preview transaction report dmo

--- model/transaction/repository.go
+++ model/transaction/repository.go
@@ -1104,13 +1104,13 @@ func (r *repository) GetListForReport(iupopkId int) (ListForCreatingReportDmoOut
 		salesSystemBargeId = append(salesSystemBargeId, v.ID)
 	}
 
-	errFindTransaction := r.db.Preload(clause.Associations).Where("report_dmo_id IS NULL AND sales_system_id IN ? AND is_finance_check = ? AND transaction_type = ? AND seller_id = ?", salesSystemBargeId, true, "DN", iupopkId).Find(&transactions).Error
+	errFindTransaction := r.db.Preload(clause.Associations).Preload("DmoDestinationPort.PortLocation").Preload("UnloadingPort.PortLocation").Where("report_dmo_id IS NULL AND sales_system_id IN ? AND is_finance_check = ? AND transaction_type = ? AND seller_id = ?", salesSystemBargeId, true, "DN", iupopkId).Find(&transactions).Error
 
 	if errFindTransaction != nil {
 		return list, errFindTransaction
 	}
 
-	errFindGroupingVessel := r.db.Preload(clause.Associations).Where("report_dmo_id IS NULL AND sales_system = ? AND iupopk_id = ?", "Vessel", iupopkId).Find(&groupingVessel).Error
+	errFindGroupingVessel := r.db.Preload(clause.Associations).Preload("DmoDestinationPort.PortLocation").Where("report_dmo_id IS NULL AND sales_system = ? AND iupopk_id = ?", "Vessel", iupopkId).Find(&groupingVessel).Error
 
 	if errFindGroupingVessel != nil {
 		return list, errFindGroupingVessel
