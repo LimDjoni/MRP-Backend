2de61d95e8fd2b7c3d52fca45bcfc329cdde9ca4
ifantrifandi <ifan.trifandi@gmail.com>
2023-09-08 19:16:40 +0700

    fix: add brackets in grouping vessel query (recap)

--- model/masterreport/repository.go
+++ model/masterreport/repository.go
@@ -118,7 +118,7 @@ func (r *repository) RecapDmo(year string, iupopkId int) (ReportDmoOutput, error
 	// Transaction Query
 	var listTransactions []transaction.Transaction
 
-	queryFilter := fmt.Sprintf("transactions.seller_id = %v AND transactions.transaction_type = 'DN' AND dmos.period LIKE '%%%v' AND transactions.dmo_id IS NOT NULL AND grouping_vessel_dns.sales_system != 'Vessel' OR grouping_vessel_dn_id IS NULL", iupopkId, year)
+	queryFilter := fmt.Sprintf("transactions.seller_id = %v AND transactions.transaction_type = 'DN' AND dmos.period LIKE '%%%v' AND transactions.dmo_id IS NOT NULL AND (grouping_vessel_dns.sales_system != 'Vessel' OR grouping_vessel_dn_id IS NULL)", iupopkId, year)
 
 	errFind := r.db.Preload("ReportDmo").Preload("DmoBuyer.IndustryType.CategoryIndustryType").Table("transactions").Select("transactions.*").Joins("left join dmos on dmos.id = transactions.dmo_id left join grouping_vessel_dns on grouping_vessel_dns.id = transactions.grouping_vessel_dn_id").Where(queryFilter).Order("shipping_date ASC").Find(&listTransactions).Error
 
