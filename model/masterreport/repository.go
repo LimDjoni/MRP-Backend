dbb64bad6adb246b37fc98bf98a34914f5beb0c1
AntoniAngga <antoniangga14@gmail.com>
2024-03-20 09:42:27 +0700

    checkpoint 1

--- model/masterreport/repository.go
+++ model/masterreport/repository.go
@@ -1394,11 +1394,14 @@ func (r *repository) RealizationReport(year string, iupopkId int) (RealizationOu
 	return realizationOutput, nil
 }
 
-func (r *repository) SaleDetailReport(year string, iupopkId int) (SaleDetail, error) {
+func (r *repository) SaleDetailReport(date string, iupopkId int) (SaleDetail, error) {
 	var saleDetail SaleDetail
 
-	startFilter := fmt.Sprintf("%v-01-01", year)
-	endFilter := fmt.Sprintf("%v-12-31", year)
+	// startFilter := fmt.Sprintf("%v-01-01", year)
+	// endFilter := fmt.Sprintf("%v-12-31", year)
+	// year := date[0:4]
+	startFilter := fmt.Sprintf("%v-01-01", date[0:4])
+	endFilter := date
 
 	companyElectricity := make(map[string][]string)
 	companyCement := make(map[string][]string)
