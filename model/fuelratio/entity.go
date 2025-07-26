185a6721d8f7407d99f45e01ec4a25b3215f9a05
LimDjoni <djoni.h@deli.id>
2025-07-26 07:32:04 +0700

    nullable on fuel_ratios date

--- model/fuelratio/entity.go
+++ model/fuelratio/entity.go
@@ -11,11 +11,11 @@ type FuelRatio struct {
 	UnitId       uint    `json:"unit_id"`
 	OperatorName string  `json:"operator_name"`
 	Shift        string  `json:"shift"`
-	Tanggal      string  `json:"tanggal"`
+	Tanggal      *string `json:"tanggal"`
 	FirstHM      float64 `json:"first_hm"`
 	LastHM       float64 `json:"last_hm"`
-	TanggalAwal  string  `json:"tanggal_awal" gorm:"DATETIME"`
-	TanggalAkhir string  `json:"tanggal_akhir" gorm:"DATETIME"`
+	TanggalAwal  *string `json:"tanggal_awal" gorm:"DATETIME"`
+	TanggalAkhir *string `json:"tanggal_akhir" gorm:"DATETIME"`
 	TotalRefill  uint    `json:"total_refill"`
 	Status       bool    `json:"status"`
 
